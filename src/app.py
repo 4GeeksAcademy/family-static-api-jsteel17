60dd5bd7c37433ee0279443827a0e18caeb888d3
jsteel17 <161091774+jsteel17@users.noreply.github.com>
2025-03-03 01:30:44 +0000

    First Commit

--- src/app.py
+++ src/app.py
@@ -25,18 +25,33 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+@app.route("/members", methods=["GET"])
+def get_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
+@app.route("/member/<int:member_id>", methods=["GET"])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        return jsonify(member), 200
+    return jsonify({"error": "Member not found"}), 404
 
-    return jsonify(response_body), 200
+@app.route("/member", methods=["POST"])
+def post_member():
+    member = request.json
+    if not all(key in member for key in ["first_name", "age", "lucky_numbers"]):
+        return jsonify({"error": "Invalid input"}), 400
+    new_member = jackson_family.add_member(member)
+    if new_member:
+        return jsonify({"message": "New member added", "member": new_member}), 200
+    return jsonify({"error": "Invalid age value"}), 400
+
+@app.route("/member/<int:member_id>", methods=["DELETE"])
+def delete_member(member_id):
+    if jackson_family.delete_member(member_id):
+        return jsonify({"done": True}), 200
+    return jsonify({"error": "Member not found"}), 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
